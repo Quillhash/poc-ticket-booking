5f2df47d8458723181a17f8c0cabb7d04377bc57
NiponChi <nipon.chi@gmail.com>
2018-07-11 13:26:11 +0700

    Added eslint

--- event/initialExpress.js
+++ event/initialExpress.js
@@ -2,17 +2,21 @@ module.exports = (config) => new Promise((resolve, reject) => {
   const express = require('express')
   const bodyParser = require('body-parser')
   const responseTime = require('response-time')
+  const cors = require('cors')
 
   const app = express()
+
+  // setup middlewares
+  app.use(cors())
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: true }))
-
   app.use(responseTime())
+
   app.listen(config.port, (err) => {
     if (err) {
       reject(err)
     } else {
-      console.log("app running on port.", config.port);
+      console.log('app running on port.', config.port)
       resolve(app)
     }
   })
