af772b4e7eafe156b456f0892e32ffd37e1a35f8
NiponChi <nipon.chi@gmail.com>
2018-07-12 00:06:57 +0700

    Updated config.js

--- event/config.js
+++ event/config.js
@@ -1,3 +1,16 @@
+const StellarSdk = require('stellar-sdk')
+
+const masterAssetCode = process.env.MEDIUM_ASSET_CODE
+const masterIssuerKey = StellarSdk.Keypair.fromSecret(process.env.MEDIUM_ASSET_ISSUER)
+const masterDistributorKey = StellarSdk.Keypair.fromSecret(process.env.MEDIUM_ASSET_DISTRIBUTOR)
+const masterAsset = new StellarSdk.Asset(masterAssetCode, masterIssuerKey.publicKey())
+
+
 module.exports = {
-  port: 3000
+  port: parseInt(process.env.PORT) || 3000,
+  masterIssuerKey,
+  masterDistributorKey,
+  masterAsset,
+  UserDbPath: process.env.USERDB_PATH || 'user.db',
+  EventDbPath: process.env.EVENTDB_PATH || 'event.db',
 }
